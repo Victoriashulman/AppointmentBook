338670243129aa76e3b1396a426a8e7bd96651bf
Victoriashulman <victorias252@nycstudents.net>
2024-11-22 16:43:22 +0000

    commit

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -9,14 +9,27 @@ public class AppointmentBook {
     }
     public int findFreeBlock(int period, int duration){
         int block = 0;
-    for(int)
+        for(int i = 0; i < 60; i++){
+            if(isMinuteFree(period, i))
+            {
+                block++;
+                if(block == duration) return i - duration + 1;
+            } else {
+                block = 0;
+            }
+        }
+        return -1;
+            
     }
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
-        return false;
+        schedule = new boolean[8][60];
+        for(int i = 25; i < 30; i++) schedule[1][i] = true;
     }
     public void printPeriod(int period)
     {
         for(int i = 0; i < schedule[period - 1].length; i++)
         System.out.println(i + " " + schedule[period - 1][i]);
     }
+    
+}
     
