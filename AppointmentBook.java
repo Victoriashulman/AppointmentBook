39cabd67a68c1e13f0c755f84a54a3b4197d3617
Victoriashulman <victorias252@nycstudents.net>
2024-12-18 16:49:43 +0000

    commit

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -3,9 +3,15 @@ public class AppointmentBook {
 
     public AppointmentBook(boolean [][] schedule){
         this.schedule = schedule; 
+        
     }
     private boolean isMinuteFree(int period, int minute){
         return schedule[period - 1][minute];
+    }
+    private void reserveBlock(int period, int startMinute, int duration){
+        for(int i = startMinute; i < startMinute + duration; i++)
+        schedule[period - 1][i] = false;
+    
     }
     public int findFreeBlock(int period, int duration){
         int block = 0;
@@ -21,15 +27,21 @@ public class AppointmentBook {
         return -1;
             
     }
-    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
-        schedule = new boolean[8][60];
-        for(int i = 25; i < 30; i++) schedule[1][i] = true;
-    }
+      
     public void printPeriod(int period)
     {
         for(int i = 0; i < schedule[period - 1].length; i++)
         System.out.println(i + " " + schedule[period - 1][i]);
     }
-    
-}
+
+    public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
+        for(int i = startPeriod; i <= endPeriod; i++)
+        int freeBlock = findFreeBlock(i, duration);
+        if(freeBlock > -1){
+            reserveBlock(i, freeBlock, duration);
+            return true;
+        }
+    return false;}
+    }    
+
     
